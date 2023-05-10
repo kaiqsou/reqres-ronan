1aa490c8a5b62a0ff2c8016ec7ffe45b5b63119f
Kaique <kaiqsouza2004@hotmail.com>
2023-05-09 22:25:26 -0300

    commit triste

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,21 +1,31 @@
+import { User } from './../models/User.model';
 import { UsersService } from './../services/users.service';
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
+import { Router, RouterLink  } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonicModule],
+  imports: [IonicModule, CommonModule, RouterLink, FormsModule],
 })
 export class HomePage {
-  constructor(private usersService: UsersService) {
+   listaUsuarios : User[] = [];
+  constructor(private usersService: UsersService, private router: Router) {
+    
+  }
+
+  ionViewWillEnter() {
     this.buscarUsuarios();
   }
-  buscarUsuarios() {
-    this.usersService.getAll().subscribe((dados) => {
-      console.log(dados);
+
+  buscarUsuarios(){
+    this.usersService.getAll().subscribe(dados => {
+      this.listaUsuarios = dados as User[];
     });
   }
 }
--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -2,33 +2,63 @@ import { Response } from './../models/Response.model';
 import { User } from '../models/User.model';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { catchError, EMPTY, map, Observable } from 'rxjs';
+import { AlertController } from '@ionic/angular';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UsersService {
-  url ="https://reqres.in/api/users?page=2"
+  url = 'https://reqres.in/api/users';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private alertCtrl: AlertController) {}
 
-  create(user : User){
-    return this.http.post(this.url, user);
+  create(user: User): Observable<User> {
+    return this.http.post<User>(this.url, user).pipe(
+      map((retorno) => retorno),
+      catchError((erro) => this.exibirErro(erro))
+    );
   }
 
-  getAll(){
-    return this.http.get(this.url)
+  getAll(): Observable<User[]> {
+    return this.http.get<User[]>(this.url).pipe(
+      map((retorno) => retorno),
+      catchError((erro) => this.exibirErro(erro))
+    );
   }
 
-  getOne(id: number){
-    return this.http.get(`${this.url}/${id}`)
+  getOne(id: number): Observable<User> {
+    return this.http.get<User>(`${this.url}/${id}`).pipe(
+      map((retorno) => retorno),
+      catchError((erro) => this.exibirErro(erro))
+    );
   }
 
-  update(user: User){
-    return this.http.put(`${this.url}/${user.id}`, user);
+  update(user: User): Observable<User> {
+    return this.http.put<User>(`${this.url}/${user.id}`, user).pipe(
+      map((retorno) => retorno),
+      catchError((erro) => this.exibirErro(erro))
+    );
   }
 
-  delete(id: number){
-    return this.http.delete(`${this.url}/${id}`)
+  delete(id: number) {
+    return this.http.delete(`${this.url}/${id}`);
   }
+  
+  exibirErro(erro: any): Observable<any> {
+    const titulo = `Erro de conexão!`;
+    const msg = `Verifique sua Conexão ou informe esse erro ao suporte: ${erro.status}`;
+    this.presentAlert(titulo, msg);
+    console.log(erro);
+    return EMPTY;
+  }
+  async presentAlert(titulo: string, msg: string) {
+    const alert = await this.alertCtrl.create({
+      header: titulo,
+      message: msg,
+      buttons: ['OK'],
+    });
 
+    await alert.present();
+  }
 }
